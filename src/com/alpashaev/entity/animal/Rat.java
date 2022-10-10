f918364cd725077c7477b01d7eab304ff76ecdce
Elementrixi <ilyas200904@gmail.com>
2022-10-10 15:33:19 +0300

    created Lynx and Rat classes with parent constr logic

---
+++ src/com/alpashaev/entity/animal/Rat.java
@@ -0,0 +1,24 @@
+package com.alpashaev.entity.animal;
+
+public class Rat extends Creature {
+
+    public Rat()
+    {
+        super(); // ref to parent class method
+        this.symbol = 'R'; //Rat symbol will be R
+        this.breedingPoint = 3; // at what point an instance of Lynx being will breed
+    }
+
+
+    public Rat(int row, int col)
+    {
+        super(row, col); // ref to parent class method
+        this.symbol = 'R'; //Rat symbol will be R
+        this.breedingPoint = 3; // at what point an instance of Lynx being will breed
+    }
+
+    public Rat(Rat a)
+    {
+        super(a);
+    }
+}
