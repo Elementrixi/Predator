297d088a6c7b641988e1f981706e93753b529654
Elementrixi <ilyas200904@gmail.com>
2022-12-17 18:19:12 +0200

    scanning for creatures, move, simul methods

--- src/com/alpashaev/manager/SimulationManager.java
+++ src/com/alpashaev/manager/SimulationManager.java
@@ -3,6 +3,7 @@ package com.alpashaev.manager;
 import com.alpashaev.entity.animal.Creature;
 import com.alpashaev.entity.animal.Lynx;
 import com.alpashaev.entity.animal.Rat;
+import com.alpashaev.map.Forest;
 
 public class SimulationManager {
     public static void start(){
@@ -43,4 +44,114 @@ public class SimulationManager {
         return creature1.rowPosition == creature2.rowPosition && creature1.colPosition == creature2.colPosition
                 && creature1.timeSinceBreed == creature2.timeSinceBreed;
     }
+
+    private static void fieldPause(){
+        try {
+            Thread.sleep(500);
+            System.out.println("Next step");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(-2);
+        }
+    }
+
+
+    private static int[][] ratPositions(){
+        int[][] pos = new int[Forest.numberOfRats][2];
+        for (int i = 0; i < Forest.numberOfRats; i++) {
+            pos[i] = Forest.ratScanner();
+        }
+        Forest.colScan=0;
+        Forest.rowScan=0;
+        return pos;
+    }
+
+    private static int[][] lynxPositions(){
+        int[][] pos = new int[Forest.numberOfLynx][2];
+        for (int i = 0; i < Forest.numberOfLynx; i++) {
+            pos[i] = Forest.lynxScanner();
+        }
+        Forest.colScan=0;
+        Forest.rowScan=0;
+        return pos;
+    }
+
+    public static void move(int[] coordinates) {
+        int row = coordinates[0];
+        int col = coordinates[1];
+
+        Creature rat = Forest.getCreature(row, col);
+        char letter = rat.getSymbol();
+        Integer[] randomDirections = Forest.randomDirections();
+        boolean moveComplete = false;
+
+        for (int i = 0; i < 4 && (moveComplete == false); i++) {
+            switch (randomDirections[i]) {
+                case 1:
+                    if (rat.topFlag)
+                        break;
+
+                    if (Forest.getCreature(coordinates[0] - 1, coordinates[1]) == null) {
+                        Forest.movement(Forest.getCreature(coordinates[0], coordinates[1]), 1);
+                        moveComplete = true;
+                    }
+                    break;
+                case 2:
+                    if (rat.rightFlag)
+                        break;
+
+                    if (Forest.getCreature(coordinates[0], coordinates[1] + 1) == null) {
+                        Forest.movement(Forest.getCreature(coordinates[0], coordinates[1]), 2);
+                        moveComplete = true;
+                    }
+                    break;
+                case 3:
+                    if (rat.bottomFlag)
+                        break;
+
+                    if (Forest.getCreature(coordinates[0] + 1, coordinates[1]) == null) {
+                        Forest.movement(Forest.getCreature(coordinates[0], coordinates[1]), 3);
+                        moveComplete = true;
+                    }
+                    break;
+                case 4:
+                    if (rat.leftFlag)
+                        break;
+
+                    if (Forest.getCreature(coordinates[0], coordinates[1] - 1) == null) {
+                        Forest.movement(Forest.getCreature(coordinates[0], coordinates[1]), 4);
+                        moveComplete = true;
+                    }
+                    break;
+            }
+        }
+    }
+
+    public static void simulation(Forest forest) {
+        int[][] lynxPositions = new int[Forest.numberOfLynx][2];
+        lynxPositions = lynxPositions();
+        int[] position = new int[2];
+
+        //Lynx movement or eat Rat if it's possible
+        for (int i = 0; i < Forest.numberOfLynx; i++) {
+            position = lynxPositions[i];
+
+            //Error catching
+            if (position[0] < 0 || position[1] < 0 || position[0] > 19 || position[1] > 19) System.exit(-1);
+
+            //Eating
+            if (forest.preyNear((Lynx) Forest.getCreature(position))) {
+                Lynx lynx = (Lynx) Forest.getCreature(position);
+                lynx.timeSinceEat = 0;
+                lynx.timeSinceBreed++;
+                lynx.eat(position);
+            } else { //Moving
+                Lynx lynx = (Lynx) Forest.getCreature(position);
+                lynx.timeSinceEat++;
+                lynx.timeSinceBreed++;
+                move(position);
+            }
+        }
+    }
+
 }
